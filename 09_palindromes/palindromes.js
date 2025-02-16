43549d8b61b87ac4db5777cee1064c63220894ca
ElTioCarlosDeAlaska <alvarez2804lucas@gmail.com>
2025-02-16 20:10:14 -0300

    finish exercises 8 to 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    text = str.toLowerCase().replaceAll(".", "").replaceAll(",", "").replaceAll("!","").replaceAll(" ", "")
+    let invertido = text.split("").reverse().join("");
+    return invertido === text
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"
+    
+    let [a,b] = [0,1]
+    for (i = 0; i < num; i++){
+        let aux = a+b
+        a = b
+        b = aux
+    } return a
 };
 
 // Do not edit below this line
